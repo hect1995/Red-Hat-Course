2383e78dabdaf34315c935cc7d064de53f3668e5
Hector Esteban <hect.esteban@gmail.com>
2020-09-17 17:12:02 +0200

    Update escribe-normas.sh

--- RH124/escribe-normas.sh
+++ RH124/escribe-normas.sh
@@ -3,7 +3,7 @@ for dir in /home/*/     # list directories in the form "/tmp/dirname/"
 do     
 	if [ ! -f ${dir}normas_nombres_ficheros.txt ]; then
 		touch ${dir}normas_nombres_ficheros.txt
-		echo ${dir} >> /var/log/escribe-norm-file.txt
+		echo ${dir#/home/} >> /var/log/escribe-norm-file.txt
 	fi 
 done
 
