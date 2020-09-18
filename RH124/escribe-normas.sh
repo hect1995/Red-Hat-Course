3106e2c1b215cc51e3649c6bf9ffe818f6e5fec2
Hector Esteban <hect.esteban@gmail.com>
2020-09-18 08:41:55 +0200

    Update escribe-normas.sh

--- RH124/escribe-normas.sh
+++ RH124/escribe-normas.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-for dir in /home/*/     # list directories in the form "/tmp/dirname/"
-do     
-	if [ ! -f ${dir}normas_nombres_ficheros.txt ]; then
-		touch ${dir}normas_nombres_ficheros.txt
-		echo ${dir#/home/} >> /var/log/escribe-norm-file.txt
-	fi 
-done
 
+FILE_SEARCH="normas_nombres_ficheros.txt"
+LOGFILE="/var/log/escribe-norm-file.txt"
+for dir in /home/*/     
+do     
+        tmp_user=${dir#/home/}
+        user=${tmp_user%/}
+        if [ ! -f ${dir}${FILE_SEARCH} ]; then
+                touch ${dir}${FILE_SEARCH}
+                chown $user ${dir}${FILE_SEARCH}
+                echo $user >> ${LOGFILE}
+        fi 
+done
